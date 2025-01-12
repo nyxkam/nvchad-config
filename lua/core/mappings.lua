e92f8ce330ef5ed8f8800543f9c1c6166c15a04b
Alex Rosas <rosasrias82@gmail.com>
2025-01-11 21:31:49 -0500

    Add  mapping coderunner and lazygit

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,4 +7,16 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<C-a>", "gg<S-v>G", { desc = "select all lines" })
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map(
+  "n",
+  "<leader>gg",
+  ":lua  require('nvchad.term').toggle { pos = 'float', id = 'fa', cmd ='lazygit' }<CR>",
+  { desc = "lazygit" }
+)
+map(
+  "n",
+  "<leader>rg",
+  ":lua  require('nvchad.term').toggle { pos = 'float', id = 'fa', cmd ='ranger' }<CR>",
+  { desc = "lazygit" }
+)
+map("n", "<C-b>", ":lua require('core.coderunner').build_run()<CR>", { desc = "Code Runner" })
