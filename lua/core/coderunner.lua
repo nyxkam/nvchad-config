202e175a14e2ec999e1c010d05d4e03deee45f7f
Alex Rosas <rosasrias82@gmail.com>
2025-01-11 21:40:56 -0500

    Add funtion coderuner for nvterm

---
+++ lua/core/coderunner.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+M.build_run = function()
+  local ft_cmds = {
+    python = "python3 " .. vim.fn.expand "%",
+    cpp = "clear && g++ -o out " .. vim.fn.expand "%" .. " && ./out",
+    c = "clear && gcc -o out " .. vim.fn.expand "%" .. " && ./out",
+    html = "live-server .",
+    js = "node " .. vim.fn.expand "%",
+    ts = "tsc " .. vim.fn.expand "%" .. " && node " .. vim.fn.expand "%:r",
+    rs = "rustc " .. vim.fn.expand "%" .. " && ./out",
+    go = "go run " .. vim.fn.expand "%",
+    java = "javac " .. vim.fn.expand "%" .. " && java " .. vim.fn.expand "%:r",
+    lua = "lua " .. vim.fn.expand "%",
+    cs = "dotnet run " .. vim.fn.expand "%",
+  }
+
+  local ft = vim.bo.filetype
+
+  local cmd = ft_cmds[ft]
+  if not cmd then
+    vim.notify("No build command configured for filetype: " .. ft, vim.log.levels.ERROR)
+    return
+  end
+
+  require("nvchad.term").runner {
+    id = "build",
+    pos = "sp",
+    cmd = cmd,
+  }
+end
+
+return M
