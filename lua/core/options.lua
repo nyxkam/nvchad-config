75b7f9a89ec7028babe36d0f95f463f924ac76d7
Alex Rosas <rosasrias82@gmail.com>
2025-01-11 21:32:29 -0500

    Change options delete unused

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,18 +2,6 @@
 
 require "nvchad.options"
 
-local opt = vim.opt
 local o = vim.o
-local g = vim.g
 
-o.cursorlineopt = 'both' -- to enable cursorline!
 o.wrap = false
-o.expandtab = true
-o.shiftwidth = 4
-o.smartindent = true
-o.tabstop = 4
-o.softtabstop = 4
-
-g.toggle_cmp = true
-g.code_action_menu_window_border = 'single'
-g.statusStyle = 'blocks' -- fancy, minimal, blocks
