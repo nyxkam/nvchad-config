75f6b5466443d23ce4966de2ec94989dd883e6f0
Alex Rosas <rosasrias82@gmail.com>
2025-01-11 21:51:23 -0500

    Add options and mappings configuration

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-    local repo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,14 +15,14 @@ local lazy_config = require "core.lazy"
 
 -- load plugins
 require("lazy").setup({
-    {
-        "NvChad/NvChad",
-        lazy = false,
-        branch = "v2.5",
-        import = "nvchad.plugins",
-    },
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
 
-    { import = "plugins" },
+  { import = "plugins" },
 }, lazy_config)
 
 -- load theme
@@ -30,6 +30,6 @@ dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 dofile(vim.g.base46_cache .. "rainbowdelimiters")
 
-require "ui"
-require "core"
+require "core.options"
+require "core.mappings"
 require "nvchad.autocmds"
