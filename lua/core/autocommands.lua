445827da4c65f703d8f5410aa810d06a324cc9a1
Alex Rosas <rosasrias82@gmail.com>
2025-01-11 21:40:31 -0500

    Delete autocommands

--- lua/core/autocommands.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("TermOpen", {
-    pattern = "*",
-    callback = function()
-        vim.opt_local.number = false
-        vim.opt_local.relativenumber = false
-    end,
-})
