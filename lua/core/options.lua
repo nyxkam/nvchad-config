8544be8d6a3d451ff14dc4337db9b3c6b23de558
Alex Diego Rosas <rosasrias82@gmail.com>
2025-01-27 08:38:00 -0500

    Update general options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,6 +2,26 @@
 
 require "nvchad.options"
 
-local o = vim.o
+local o = vim.opt
 
 o.wrap = false
+o.emoji = true
+o.ruler = true
+o.history = 1000
+o.fillchars = {
+  stlnc = " ",
+  diff = "░",
+  msgsep = "━",
+  foldopen = "",
+  foldsep = "│",
+  foldclose = "",
+  horiz = "━",
+  horizup = "┻",
+  horizdown = "┳",
+  vert = "┃",
+  vertleft = "┫",
+  vertright = "┣",
+  verthoriz = "╋",
+  fold = "─",
+  eob = " ",
+}
